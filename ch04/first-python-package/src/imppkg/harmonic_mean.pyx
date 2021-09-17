6f5133667dec3ecab593f1e9d2a4f18a0ee3ab8c
Dane Hillard <github@danehillard.com>
2021-09-17 08:51:03 -0400

    Use Python syntax instead of Cython

--- ch04/first-python-package/src/imppkg/harmonic_mean.pyx
+++ ch04/first-python-package/src/imppkg/harmonic_mean.pyx
@@ -1,23 +1,2 @@
-from libc.stdlib cimport malloc, free
-
-
 def harmonic_mean(nums):
-    cdef int length = len(nums)
-    cdef double denominator = 0
-    cdef int i
-
-    cdef double* cnums = <double*>malloc(length * sizeof(double))
-
-    if not cnums:
-        raise MemoryError()
-
-    try:
-        for i in range(length):
-            cnums[i] = nums[i]
-
-        for i in range(length):
-            denominator += 1 / cnums[i]
-
-        return length / denominator
-    finally:
-        free(cnums)
+    return len(nums) / sum(1 / num for num in nums)
